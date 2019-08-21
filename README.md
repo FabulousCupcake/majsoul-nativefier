926cbb6d764b0d22f63ad42d8cf788eece8d4e48
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2019-08-21 21:56:04 +0200

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Majsoul Nativefier
+This repository contains [nativefier][1] configuration to wrap [Mahjong Soul][2] with Electron.
+This repository uses a [forked nativefier][3] to allow additional electron configuration to modify default fonts.
+
+## Usage
+```bash
+# Install dependencies
+npm install
+
+# Run nativefier
+npm run build
+```
+
+[1]: https://github.com/jiahaog/nativefier
+[2]: https://mahjongsoul.game.yo-star.com/
+[3]: https://github.com/FabulousCupcake/nativefier
---
+++ index.js
@@ -0,0 +1,43 @@
+const nativefier = require('nativefier').default;
+
+const W = 1280;
+const H = 740;
+// Mahjong Soul is perfectly 16:9, so this should be 720
+// Here we're adding 20 to account for title bar in OSX
+
+var options = {
+    name: 'Mahjong Soul',
+    targetUrl: 'https://mahjongsoul.game.yo-star.com/',
+    out: './dist',
+    overwrite: true,
+    asar: true,
+
+    width: W,
+    height: H,
+    minWidth: W,
+    minHeight: H,
+    maxWidth: W,
+    maxHeight: H,
+
+    disableDevTools: true,
+    fastQuit: true,
+    singleInstance: true,
+    showMenuBar: false,
+
+    browserwindowOptions: {
+        webPreferences: {
+            defaultFontFamily: {
+                standard: "Comic Sans MS",
+                serif: "Comic Sans MS",
+            }
+        }
+    },
+};
+
+nativefier(options, function(error, appPath) {
+    if (error) {
+        console.error(error);
+        return;
+    }
+    console.log('App has been nativefied to', appPath);
+});
